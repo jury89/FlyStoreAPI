8963f87fe1785c5f0b60f931e526dcff7c58045e
Jury D'Ambros <jury.dambros@inflyter.com>
2024-07-12 12:49:56 +0200

    endpoint get user by id

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Union
 
 from fastapi import FastAPI, Depends, HTTPException
@@ -35,3 +36,11 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def get_user(user_id: uuid.UUID, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
